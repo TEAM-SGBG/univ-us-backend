1886e0f6610e0216e8577234b4bdf73ecc3d4d70
geonjunk <koeunjun7@gmail.com>
2021-10-29 11:37:43 +0900

    geonjunk/database.js rds 연결추가

--- routes/database.js
+++ routes/database.js
@@ -5,10 +5,10 @@ require('dotenv').config({path: path.join(__dirname, "../credentials/.env")}); /
 const connectDB = async()=>{
     try{
         const conn=mysql.createConnection({
-            host: 'localhost',//사용할 db가 설치된 호스트 ip
+            host: process.env.END_POINT,//사용할 db가 설치된 호스트 ip
             port: '3306',//DB설치할 때 사용자가 지정한 포트번호 기본값 3306
-            user: 'root',//DB의 user이름
-            password: process.env.MYSQL_PW,//비번
+            user: 'admin',//DB의 user이름
+            password: process.env.PW,//비번
             database: 'univus'//사용할 데이터베이스 명
         });
         conn.connect((err)=>{
@@ -21,3 +21,8 @@ const connectDB = async()=>{
 }
 
 module.exports = connectDB;
+
+// crud 
+// 채널 생성 crud
+// 행사 생성 crud
+
