591cd8130758f3a9c3126b3541ca937226e9c9c0
geonjunk <koeunjun7@gmail.com>
2021-12-08 14:24:24 +0900

    change url

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const passport=require(`passport`);
 
 
 const router=express.Router();
-
+const myUrl=`https://univ-us.netlify.app`;
 
 router.get('/',isLogin,(req,res)=>{//로그인 상태확인
     res.send(req.user);
@@ -13,7 +13,7 @@ router.get('/',isLogin,(req,res)=>{//로그인 상태확인
 router.get('/logout',(req,res)=>{
     req.logout();
     req.session.destroy();
-    res.redirect(`http://localhost:3000`);//home으로 redirect
+    res.redirect(myUrl);//home으로 redirect
 });
 
 router.get('/kakao',(req,res,next)=>{
@@ -22,13 +22,13 @@ router.get('/kakao',(req,res,next)=>{
 },passport.authenticate('kakao'));//카카오 로그인 시작
 
 
-router.get('/kakao/callback',passport.authenticate('kakao',{failureRedirect:'http://localhost:3000',}),//로그인 성공여부 결과 받음
+router.get('/kakao/callback',passport.authenticate('kakao',{failureRedirect:myUrl,}),//로그인 성공여부 결과 받음
 (req,res)=>{
     console.log(`로그인완료`);
         //console.log(req.session);//사용자 전체정보
         //req.user
     //req.session.passport.user//사용자 id만 
-    res.redirect('http://localhost:3000'+req.session.redirect);
+    res.redirect(myUrl+req.session.redirect);
 });
 //kakao 로그인은내부적으로 req.login호출하므로 직접 호출할 필요없음
 
@@ -40,10 +40,10 @@ router.get(`/google`,(req,res,next)=>{
 
 router.get(
     '/google/callback',
-    passport.authenticate('google', {failureRedirect: 'http://localhost:3000'}), 
+    passport.authenticate('google', {failureRedirect: myUrl}), 
     (req, res) => {
     console.log("로그인 완료");
-    res.redirect('http://localhost:3000'+req.session.redirect);
+    res.redirect(myUrl+req.session.redirect);
 });
   
 module.exports=router;
