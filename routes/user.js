04115cf5f7cb2446972c186472cc1d169486c138
geonjunk <koeunjun7@gmail.com>
2021-12-10 17:05:28 +0900

    구독한 채널만 있는 경우 삭제가능

--- routes/user.js
+++ routes/user.js
@@ -26,31 +26,42 @@ router.get('/all_user', (req, res) => {
 })
 // 2. 유저 삭제
 router.delete('/delete_user/:user_id', (req, res) => {
-    conn.query(`DELETE FROM user WHERE id_token = ?`,[req.params.user_id],(err,result)=>{
-        if(err){
+    conn.query(`DELETE FROM channel_subscriber WHERE subscriber_id=?`,[req.params.user_id],(err1,result1)=>{
+        if(err1){
             res.status(400).json({
                 success: false,
-                err: err,
+                err: err1,
                 message: 'ERROR user/delete_user'
             })
-        }
-        else{
-            if(result.affectedRows==0){
-                res.status(400).json({
-                    success: false,
-                    err: '삭제실패',
-                    message: 'ERROR user/delete_user'
-                })
-            }else{
-                res.status(200).json({
-                    success: true,
-                    message: 'SUCCESS user/delete_user',
-                    data: result
-                })
-            }
-         
+        }else{
+            conn.query(`DELETE FROM user WHERE id_token = ?`,[req.params.user_id],(err2,result2)=>{
+                if(err2){
+                    res.status(400).json({
+                        success: false,
+                        err: err2,
+                        message: 'ERROR user/delete_user'
+                    })
+                }
+                else{
+                    if(result2.affectedRows==0){
+                        res.status(400).json({
+                            success: false,
+                            err: '삭제실패',
+                            message: 'ERROR user/delete_user'
+                        })
+                    }else{
+                        res.status(200).json({
+                            success: true,
+                            message: 'SUCCESS user/delete_user',
+                            data: result2
+                        })
+                    }
+                 
+                }
+            })
         }
     })
+
 })
 
 
