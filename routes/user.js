cbd0d372572b0ae98fc5e60acac4d531663d3404
geonjunk <koeunjun7@gmail.com>
2021-12-10 13:51:18 +0900

    user 삭제

--- routes/user.js
+++ routes/user.js
@@ -25,8 +25,8 @@ router.get('/all_user', (req, res) => {
     })
 })
 // 2. 유저 삭제
-router.post('/delete_user', (req, res) => {
-    conn.query(`DELETE FROM user WHERE id_token=?`,[req.id_token],(err,result)=>{
+router.delete('/delete_user/:user_id', (req, res) => {
+    conn.query(`DELETE FROM user WHERE id_token = ?`,[req.params.user_id],(err,result)=>{
         if(err){
             res.status(400).json({
                 success: false,
@@ -35,13 +35,23 @@ router.post('/delete_user', (req, res) => {
             })
         }
         else{
-            res.status(200).json({
-                success: true,
-                message: 'SUCCESS user/delete_user',
-                data: result
-            })
+            if(result.affectedRows==0){
+                res.status(400).json({
+                    success: false,
+                    err: '삭제실패',
+                    message: 'ERROR user/delete_user'
+                })
+            }else{
+                res.status(200).json({
+                    success: true,
+                    message: 'SUCCESS user/delete_user',
+                    data: result
+                })
+            }
+         
         }
     })
 })
+
 
 module.exports = router; 
